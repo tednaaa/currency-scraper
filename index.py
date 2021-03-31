27f474c06c56b1455508781726a44dc1f66a9e29
and-bulk <13and.bulk@gmail.com>
2021-03-31 12:19:38 +0400

    migrate to selenium

--- index.py
+++ index.py
@@ -1,15 +1,44 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import json
 
-def get_bitcoin(currency):
-	url = f'https://alpari.com/ru/markets/crypto/bitcoin/{currency}'
+def infinite_scroll(driver):
+  show_more_button = driver.find_element_by_xpath('//div[@class="showMoreindex___2jfzg"]/span')
 
-	res = requests.get(url)
+  while True:
+    try:
+      driver.execute_script('document.querySelector(".showMoreindex___2jfzg").scrollIntoView();')
+      sleep(0.1)
+      show_more_button.click()
+    except:
+      break
 
-	soup = BeautifulSoup(res.text, 'html.parser')
+def get_bitcoin(driver):
+  store = []
 
-	bitcoin = soup.find('div', {'class': 'analytics-crypto-item__instrument-price-left'}).text
+  URL = 'https://exmo.me'
+  driver.get(URL)
+  
+  infinite_scroll(driver)
 
-	return bitcoin
+  currency_names = driver.find_elements_by_xpath('//h4[@class="currencyNameTextindex___D63V7"]')
+  prices = driver.find_elements_by_xpath('//div[@class="lastPriceindex___3WMfX"]/div[1]')
+  prices_usd = driver.find_elements_by_xpath('//div[@class="lastPriceindex___3WMfX"]/div[2]')
+  prices_changes_percent = driver.find_elements_by_class_name('changeFor24hindex___3ek4T')
 
-print(get_bitcoin('usd'))
+  for currency_name, price, price_usd, price_changes_percent in zip(currency_names, prices, prices_usd, prices_changes_percent):
+    store.append({
+      'name': currency_name.text,
+      'price': f'{price.text} {price_usd.text}',
+      'changes_percent': price_changes_percent.text
+    })
+
+  with open('db.json', 'w', encoding = 'utf-8') as database_file:
+    json.dump(store, database_file, indent = 2, ensure_ascii = False)
+
+def main():
+  driver = webdriver.Chrome('./drivers/chromeDriverWindows')
+  get_bitcoin(driver)
+
+if __name__ == '__main__':
+  main()
