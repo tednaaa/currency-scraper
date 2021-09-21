cccd06090a1e34126d6fcbfc31860329aa4c5598
And Grigoryan <13and.bulk@gmail.com>
2021-09-21 11:11:43 +0400

    added cross-platform path and fixed css selectors

--- app/config/config.py
+++ app/config/config.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 import os
 
 URL = os.getenv('URL')
-CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
-SAVE_DATA_DIRECTORY_PATH = os.getenv('SAVE_DATA_DIRECTORY_PATH')
+CHROME_DRIVER_PATH = os.path.join(
+    os.getcwd(), 'drivers', os.getenv('CHROME_DRIVER'))
+SAVE_DATA_DIRECTORY_PATH = os.path.join(os.getcwd(), 'data')
 BITCOIN_CURRENCY_FILE_NAME = 'bitcoin-currency.json'
 CURRENCY_NAMES_XPATH = os.getenv('CURRENCY_NAMES_XPATH')
 PRICES_XPATH = os.getenv('PRICES_XPATH')
 PRICES_IN_USD_XPATH = os.getenv('PRICES_IN_USD_XPATH')
 PRICES_CHANGES_PERCENT_CLASS_NAME = os.getenv(
     'PRICES_CHANGES_PERCENT_CLASS_NAME')
+
 
 WEB_DRIVER = webdriver.Chrome(CHROME_DRIVER_PATH)
